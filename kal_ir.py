ef00a9dab1338be14bda40fae86b100d52925261
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-06-21 15:49:56 -0300

    Rename 'visit' to 'emit'

--- kal_ir.py
+++ kal_ir.py
@@ -6,28 +6,19 @@ import kal_ast
 import kal_ops
 
 
-class NodeVisitor:
-    """ Base class for visiting AST nodes. """
-
-    def _visit(self, node: kal_ast.Node) -> Any:
-        """ Visit a node. """
-        method = f"_visit_{node.__class__.__name__}"
-        visitor = getattr(self, method)
-        return visitor(node)
-
-
 class GenerateCodeError(Exception):
     pass
 
 
+# NOTE Kaleidoscope uses double precision floating point for all values
 ZERO = FALSE = ir.Constant(ir.DoubleType(), 0.0)
 ONE = TRUE = ir.Constant(ir.DoubleType(), 1.0)
 
 
-class LLVMCodeGenerator(NodeVisitor):
+class LLVMCodeGenerator:
     """ Node visitor class that generates LLVM IR code.
 
-        Note: each `_visit_<Node>()` method should return an appropriate `ir.Value`.
+        Note: each `_emit_<Node>()` method should return an appropriate `ir.Value`.
     """
 
     def __init__(self):
@@ -38,23 +29,29 @@ class LLVMCodeGenerator(NodeVisitor):
         self.builder: ir.IRBuilder = None
 
         # Manages a symbol table while a function is being visited
-        self.symtab: Dict[str, ir.Value] = {}
+        self.func_symtab: Dict[str, ir.Value] = {}
 
     def generate_code(self, node: kal_ast.Node):
         assert isinstance(node, (kal_ast.Prototype, kal_ast.Function))
-        return self._visit(node)
+        return self._emit(node)
 
-    def _visit_NumberExpr(self, node: kal_ast.NumberExpr) -> ir.Value:
+    def _emit(self, node: kal_ast.Node) -> Any:
+        """ Visit an AST node and emit its corresponding LLVM IR. """
+        method = f"_emit_{node.__class__.__name__}"
+        visitor = getattr(self, method)
+        return visitor(node)
+
+    def _emit_NumberExpr(self, node: kal_ast.NumberExpr) -> ir.Value:
         return ir.Constant(ir.DoubleType(), float(node.val))
 
-    def _visit_VariableExpr(self, node: kal_ast.VariableExpr) -> ir.Value:
-        if (value := self.symtab.get(node.name)) is not None:
+    def _emit_VariableExpr(self, node: kal_ast.VariableExpr) -> ir.Value:
+        if (value := self.func_symtab.get(node.name)) is not None:
             return value
         raise GenerateCodeError(f"Unknown variable name '{node.name}'")
 
-    def _visit_BinaryExpr(self, node: kal_ast.BinaryExpr) -> ir.Value:
-        lhs = self._visit(node.lhs)
-        rhs = self._visit(node.rhs)
+    def _emit_BinaryExpr(self, node: kal_ast.BinaryExpr) -> ir.Value:
+        lhs = self._emit(node.lhs)
+        rhs = self._emit(node.rhs)
 
         if node.op == "+":
             return self.builder.fadd(lhs, rhs, name="addtmp")
@@ -73,8 +70,8 @@ class LLVMCodeGenerator(NodeVisitor):
 
         raise GenerateCodeError(f"Unknown binary operator '{node.op}'")
 
-    def _visit_UnaryExpr(self, node: kal_ast.UnaryExpr) -> ir.Value:
-        operand = self._visit(node.operand)
+    def _emit_UnaryExpr(self, node: kal_ast.UnaryExpr) -> ir.Value:
+        operand = self._emit(node.operand)
 
         # NOTE There are no pre-defined unary operators (unlike with binary operators)
         if (user_def_un_op_fn := self.module.globals.get(f"unary{node.op}")) is not None:
@@ -83,8 +80,8 @@ class LLVMCodeGenerator(NodeVisitor):
 
         raise GenerateCodeError(f"Unknown unary operator '{node.op}'")
 
-    def _visit_IfExpr(self, node: kal_ast.IfExpr) -> ir.Value:
-        cond_value = self._visit(node.cond_expr)
+    def _emit_IfExpr(self, node: kal_ast.IfExpr) -> ir.Value:
+        cond_value = self._emit(node.cond_expr)
         # NOTE ordered means that neither operand can be a QNAN (quite NaN)
         fcmp = self.builder.fcmp_ordered(cmpop="!=", lhs=cond_value, rhs=FALSE, name="ifcond",)
 
@@ -97,7 +94,7 @@ class LLVMCodeGenerator(NodeVisitor):
         # Emit the 'then' block
         self.builder.function.basic_blocks.append(then_bb)
         self.builder.position_at_start(block=then_bb)
-        then_value = self._visit(node.then_expr)
+        then_value = self._emit(node.then_expr)
         self.builder.branch(target=merge_bb)  # branch to 'endif' after executing 'then'
 
         # NOTE Emission of then_value can modify the current basic block, so we
@@ -107,7 +104,7 @@ class LLVMCodeGenerator(NodeVisitor):
         # Emit the 'else' block
         self.builder.function.basic_blocks.append(else_bb)
         self.builder.position_at_start(block=else_bb)
-        else_value = self._visit(node.else_expr)
+        else_value = self._emit(node.else_expr)
         self.builder.branch(target=merge_bb)  # branch to 'endif' after executing 'else'
 
         # NOTE Emission of else_value can modify the current basic block, so we
@@ -122,9 +119,9 @@ class LLVMCodeGenerator(NodeVisitor):
         phi.add_incoming(value=else_value, block=else_bb)
         return phi
 
-    def _visit_ForExpr(self, node: kal_ast.ForExpr) -> ir.Value:
+    def _emit_ForExpr(self, node: kal_ast.ForExpr) -> ir.Value:
         # Emit the initializer code first, without the loop variable in scope
-        init_value = self._visit(node.init_expr)
+        init_value = self._emit(node.init_expr)
 
         # Make the new basic block for the loop header, inserting it after the current block
         preheader_bb = self.builder.block
@@ -143,24 +140,24 @@ class LLVMCodeGenerator(NodeVisitor):
 
         # Within the loop, the variable is defined equal to the PHI node
         # If it shadows an existing variable, we have to restore it, so we save it now
-        old_value = self.symtab.get(node.id_name)
-        self.symtab[node.id_name] = phi
+        old_value = self.func_symtab.get(node.id_name)
+        self.func_symtab[node.id_name] = phi
 
         # Emit the body of the loop
         # NOTE This, like any other expr, can change the current BB
-        _body_value = self._visit(node.body_expr)  # NOTE we ignore the computed value
+        _body_value = self._emit(node.body_expr)  # NOTE we ignore the computed value
 
         # Emit the step value
         step_value = (
             ONE  # if not specified, use 1.0
             if node.step_expr is None
-            else self._visit(node.step_expr)
+            else self._emit(node.step_expr)
         )
 
         next_loop_var_value = self.builder.fadd(phi, step_value, "nextloopvar")
 
         # Compute the end-loop condition and convert it to a bool
-        cond_value = self._visit(node.cond_expr)
+        cond_value = self._emit(node.cond_expr)
         fcmp = self.builder.fcmp_ordered(cmpop="!=", lhs=cond_value, rhs=FALSE, name="loopcond")
 
         # Create the "after loop" block ('endfor') and insert it
@@ -179,61 +176,59 @@ class LLVMCodeGenerator(NodeVisitor):
 
         # Remove the loop variable from the symbol table
         if old_value is None:
-            del self.symtab[node.id_name]
+            del self.func_symtab[node.id_name]
         else:
-            self.symtab[node.id_name] = old_value  # restore the shadowed variable
+            self.func_symtab[node.id_name] = old_value  # restore the shadowed variable
 
         # The 'for' expression always returns 0.0
         return ZERO
 
-    def _visit_CallExpr(self, node: kal_ast.CallExpr) -> ir.Value:
+    def _emit_CallExpr(self, node: kal_ast.CallExpr) -> ir.Value:
         # Look up the name in the global module table
-        callee_fn = self.module.globals.get(node.callee, None)
+        callee_fn = self.module.globals.get(node.callee)
 
-        if callee_fn is None or not isinstance(callee_fn, ir.Function):
+        if not isinstance(callee_fn, ir.Function):
             raise GenerateCodeError(f"Call to unknown function '{node.callee}'")
         elif len(callee_fn.args) != len(node.args):
             raise GenerateCodeError(f"Call argument length mismatch for '{node.callee}'")
 
-        call_args = [self._visit(arg) for arg in node.args]
-        return self.builder.call(fn=callee_fn, args=call_args, name="calltmp")
+        return self.builder.call(
+            fn=callee_fn, args=[self._emit(arg) for arg in node.args], name="calltmp"
+        )
 
-    def _visit_Prototype(self, node: kal_ast.Prototype) -> ir.Value:
+    def _emit_Prototype(self, node: kal_ast.Prototype) -> ir.Value:
         fn_name = node.name
         fn_type = ir.FunctionType(
             return_type=ir.DoubleType(), args=[ir.DoubleType() for _ in range(len(node.params))],
-        )  # NOTE Kaleidoscope uses double precision floating point for all values
+        )
 
-        if fn_name in self.module.globals:
-            fn = self.module.globals[fn_name]
-            if not isinstance(fn, ir.Function):
-                raise GenerateCodeError(f"Function/global name collision '{fn_name}'")
-            elif not fn.is_declaration:
-                # TODO allow function redefinition on the REPL
-                raise GenerateCodeError(f"Redefinition of '{fn_name}'")
-            elif len(fn.function_type.args) != len(fn_type.args):
-                raise GenerateCodeError(f"Definition of '{fn_name}' with wrong argument count")
-        else:
+        if (fn := self.module.globals.get(fn_name)) is None:
             # Create a new function and set its arguments names
             fn = ir.Function(module=self.module, ftype=fn_type, name=fn_name)
             for arg, arg_name in zip(fn.args, node.params):
                 arg.name = arg_name
-                self.symtab[arg.name] = arg
+                self.func_symtab[arg.name] = arg
+
+        elif not isinstance(fn, ir.Function):
+            raise GenerateCodeError(f"Function/global name collision '{fn_name}'")
+        elif not fn.is_declaration:
+            raise GenerateCodeError(f"Redefinition of '{fn_name}'")
+        elif len(fn.function_type.args) != len(fn_type.args):
+            raise GenerateCodeError(f"Definition of '{fn_name}' with wrong argument count")
 
         return fn
 
-    def _visit_Function(self, node: kal_ast.Function) -> ir.Value:
-        # NOTE prototype generation will pre-populate symtab with function arguments
-        self.symtab = {}
-
-        fn: ir.Function = self._visit(node.proto)
+    def _emit_Function(self, node: kal_ast.Function) -> ir.Value:
+        # NOTE prototype generation will pre-populate func_symtab with function arguments
+        self.func_symtab = {}
+        fn: ir.Function = self._emit(node.proto)
 
         # Create a new basic block to start insertion into
         bb_entry = fn.append_basic_block(name="entry")
         self.builder = ir.IRBuilder(block=bb_entry)
 
         # Finish off the function
-        fn_return_value: ir.Value = self._visit(node.body)
+        fn_return_value: ir.Value = self._emit(node.body)
         self.builder.ret(value=fn_return_value)
 
         return fn
